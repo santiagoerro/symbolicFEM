a97dd4e5d9fcc8de2eb63ff2236dc9e882451f6c
santiagoerro <santiagoea01@gmail.com>
2026-04-30 19:33:27 +0200

    Added accurate calculation of torsion and horizontal bending-torsion coupling mass matrices, with correct prefactors and series expansions around a = 0 for all expressions.

--- timoshenkoBending.py
+++ timoshenkoBending.py
@@ -14,34 +14,46 @@ zNeutralAxis = sym.Symbol('z_n')
 zCenterOfTwist = sym.Symbol('z_t')
 verticalMassInertiaMoment = sym.Symbol('I_yy')
 horizontalMassInertiaMoment = sym.Symbol('I_zz')
+rollMassInertiaMoment = sym.Symbol('I_xx')
+
+# C11w0 = sym.Symbol('C11w0')
+# C12w0 = sym.Symbol('C12w0')
+# C13w0 = sym.Symbol('C13w0')
+# C14w0 = sym.Symbol('C14w0')
+# C11w1 = sym.Symbol('C11w1')
+# C12w1 = sym.Symbol('C12w1')
+# C13w1 = sym.Symbol('C13w1')
+# C14w1 = sym.Symbol('C14w1')
 
 x = sym.Symbol('x')
 
-printType = 'python'
+printType = 'pretty'
 
 
 
-def PrettyPrint(expression, factor = None):
-    if factor is None:
-        if printType == 'latex':
-            print(sym.latex(sym.simplify(expression)))
-        elif printType == 'python':
-            print(sym.python(sym.simplify(expression)))
-        else:
-            sym.pprint(sym.simplify(expression))
+def SimplifyPrint(expression, factor = None, series = False, symbol = sym.Symbol('x'), x0 = 0, order = 10):
+    if printType == 'latex':
+        Printer = lambda x: print(sym.latex(x))
+    elif printType == 'python':
+        Printer = lambda x: print(sym.python(x))
     else:
-        if printType == 'latex':
-            print(sym.latex(sym.simplify(factor)))
+        Printer = sym.pprint
+
+    if factor is None:
+        simplifiedExpression = sym.simplify(expression)
+        Printer(simplifiedExpression)
+        if series:
+            Printer(sym.series(simplifiedExpression, symbol, x0, order))
+    else:
+        simplifiedFactor = sym.simplify(factor)
+        simplifiedFactoredExpression = sym.simplify(expression / factor)
+        Printer(simplifiedFactor)
+        print('*')
+        Printer(simplifiedFactoredExpression)
+        if series:
+            Printer(simplifiedFactor)
             print('*')
-            print(sym.latex(sym.simplify(expression / factor)))
-        elif printType == 'python':
-            print(sym.python(sym.simplify(factor)))
-            print('*')
-            print(sym.python(sym.simplify(expression / factor)))
-        else:
-            sym.pprint(sym.simplify(factor))
-            print('*')
-            sym.pprint(sym.simplify(expression / factor))
+            Printer(sym.series(simplifiedFactoredExpression, symbol, x0, order))
 
 
 
@@ -104,7 +116,8 @@ axialMassMatrix: sym.Matrix = linearDensity * sym.integrate(axialDisplacementDis
 verticalBendingHeaveContributionMassMatrix: sym.Matrix = linearDensity * sym.integrate(verticalDeflectionDisplacementBasis.transpose() * verticalDeflectionDisplacementBasis, (x, 0, l))
 verticalBendingPitchContributionMassMatrix: sym.Matrix = (verticalMassInertiaMoment + linearDensity * (zCenterOfMass - zNeutralAxis)**2) * sym.integrate(pitchRotationDisplacementBasis.transpose() * pitchRotationDisplacementBasis, (x, 0, l))
 axialVerticalBendingCouplingMassMatrix: sym.Matrix = linearDensity * (zCenterOfMass - zNeutralAxis) * sym.integrate(axialDisplacementDisplacementBasis.transpose() * pitchRotationDisplacementBasis, (x, 0, l))
-horizontalBendingMassMatrix: sym.Matrix = linearDensity * sym.integrate(horizontalDeflectionDisplacementBasis.transpose() * horizontalDeflectionDisplacementBasis, (x, 0, l))
+horizontalBendingSwayContributionMassMatrix: sym.Matrix = linearDensity * sym.integrate(horizontalDeflectionDisplacementBasis.transpose() * horizontalDeflectionDisplacementBasis, (x, 0, l))
+horizontalBendingYawContributionMassMatrix: sym.Matrix = horizontalMassInertiaMoment * sym.integrate(yawRotationDisplacementBasis.transpose() * yawRotationDisplacementBasis, (x, 0, l))
 
 
 twistingBoundedBasis: sym.Matrix = sym.Matrix([1, x/l, sym.exp(a * (x/l - 1)), sym.exp(-a * x/l)]).transpose()
@@ -116,92 +129,115 @@ boundedBasisCoefsToConditionsMatrix[2, :] = sym.diff(twistingBoundedBasis, x).su
 boundedBasisCoefsToConditionsMatrix[3, :] = sym.diff(twistingBoundedBasis, x).subs(x, l) * l
 stableBasisToBoundedBasisCoefsMatrix: sym.Matrix = sym.zeros(4,4)
 stableBasisToBoundedBasisCoefsMatrix[:, 0] = sym.Matrix([1, -1, 0, 0])
-stableBasisToBoundedBasisCoefsMatrix[:, 1] = sym.Matrix([0, 1, 0, 0])
-stableBasisToBoundedBasisCoefsMatrix[:, 2] = sym.simplify(boundedBasisCoefsToConditionsMatrix.LDLsolve(sym.Matrix([0, 0, 1, 0])))
+stableBasisToBoundedBasisCoefsMatrix[:, 1] = sym.simplify(boundedBasisCoefsToConditionsMatrix.LDLsolve(sym.Matrix([0, 0, 1, 0])))
+stableBasisToBoundedBasisCoefsMatrix[:, 2] = sym.Matrix([0, 1, 0, 0])
 stableBasisToBoundedBasisCoefsMatrix[:, 3] = sym.simplify(boundedBasisCoefsToConditionsMatrix.LDLsolve(sym.Matrix([0, 0, 0, 1])))
 
-# twistingStableBasis: sym.Matrix = twistingBoundedBasis * stableBasisToBoundedBasisCoefsMatrix
-# PrettyPrint(twistingStableBasis)
+# stableBasisToBoundedBasisCoefsMatrix: sym.Matrix = sym.zeros(4,4)
+# stableBasisToBoundedBasisCoefsMatrix[:, 0] = sym.Matrix([1, -1, 0, 0])
+# stableBasisToBoundedBasisCoefsMatrix[:, 1] = sym.Matrix([C11w0, C12w0, C13w0, C14w0])
+# stableBasisToBoundedBasisCoefsMatrix[:, 2] = sym.Matrix([0, 1, 0, 0])
+# stableBasisToBoundedBasisCoefsMatrix[:, 3] = sym.Matrix([C11w1, C12w1, C13w1, C14w1])
 
-massMatrixBoundedBasis = sym.integrate(twistingBoundedBasis.transpose() * twistingBoundedBasis, (x, 0, l))
+massMatrixBoundedBasis = (rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2) * sym.integrate(twistingBoundedBasis.transpose() * twistingBoundedBasis, (x, 0, l))
 massMatrixStableBasis = sym.simplify(stableBasisToBoundedBasisCoefsMatrix.transpose() * massMatrixBoundedBasis * stableBasisToBoundedBasisCoefsMatrix)
 
-
-bendingverticalDeflectionDisplacementBasisTwistingBoundedBasis = sym.Matrix.hstack(verticalDeflectionDisplacementBasis, twistingBoundedBasis)
-
-bendingTorsionCoupledMassMatrixDisplacementBoundedBasis = sym.integrate(bendingverticalDeflectionDisplacementBasisTwistingBoundedBasis.transpose() * bendingverticalDeflectionDisplacementBasisTwistingBoundedBasis, (x, 0, l))
-
+horizontalBendingDispBasisTorsionBoundedBasisCouplingMassMatrix: sym.Matrix = -linearDensity * (zCenterOfMass - zCenterOfTwist) * sym.integrate(horizontalDeflectionDisplacementBasis.transpose() * twistingBoundedBasis, (x, 0, l))
+horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix: sym.Matrix = sym.simplify(horizontalBendingDispBasisTorsionBoundedBasisCouplingMassMatrix * stableBasisToBoundedBasisCoefsMatrix)
 
 
 print()
 print('Axial force:')
-PrettyPrint(axialForceDisplacementBasis, factor = EA/l)
+SimplifyPrint(axialForceDisplacementBasis, factor = EA/l)
 print()
 print('Axial stiffness matrix:')
-PrettyPrint(axialStiffnessMatrix, factor = EA/l)
+SimplifyPrint(axialStiffnessMatrix, factor = EA/l)
 print()
 print('Vertical bending moment:')
-PrettyPrint(verticalBendingMomentDisplacementBasis, factor = EIVertical / l**2 / (1 + shearCorrectionFactorVertical))
+SimplifyPrint(verticalBendingMomentDisplacementBasis, factor = EIVertical / l**2 / (1 + shearCorrectionFactorVertical))
 print()
 print('Horizontal bending moment:')
-PrettyPrint(horizontalBendingMomentDisplacementBasis, factor = EIHorizontal / l**2 / (1 + shearCorrectionFactorHorizontal))
+SimplifyPrint(horizontalBendingMomentDisplacementBasis, factor = EIHorizontal / l**2 / (1 + shearCorrectionFactorHorizontal))
 print()
 print('Vertical shear force:')
-PrettyPrint(verticalShearForceDisplacementBasis, factor = EIVertical / l**3 / (1 + shearCorrectionFactorVertical))
+SimplifyPrint(verticalShearForceDisplacementBasis, factor = EIVertical / l**3 / (1 + shearCorrectionFactorVertical))
 print()
 print('Horizontal shear force:')
-PrettyPrint(horizontalShearForceDisplacementBasis, factor = EIHorizontal / l**3 / (1 + shearCorrectionFactorHorizontal))
+SimplifyPrint(horizontalShearForceDisplacementBasis, factor = EIHorizontal / l**3 / (1 + shearCorrectionFactorHorizontal))
 print()
 print('Vertical bending stiffness matrix:')
-PrettyPrint(verticalStiffnessMatrix, factor = EIVertical / l**2 / (1 + shearCorrectionFactorVertical))
+SimplifyPrint(verticalStiffnessMatrix, factor = EIVertical / l**2 / (1 + shearCorrectionFactorVertical))
 print()
 print('Horizontal bending stiffness matrix:')
-PrettyPrint(horizontalStiffnessMatrix, factor = EIHorizontal / l**2 / (1 + shearCorrectionFactorHorizontal))
+SimplifyPrint(horizontalStiffnessMatrix, factor = EIHorizontal / l**2 / (1 + shearCorrectionFactorHorizontal))
 print()
 print('Axial mass matrix:')
-PrettyPrint(axialMassMatrix, factor = l * linearDensity)
+SimplifyPrint(axialMassMatrix, factor = l * linearDensity)
 print()
 print('Vertical bending mass matrix:')
 print('Heave contribution:')
-PrettyPrint(verticalBendingHeaveContributionMassMatrix, factor = l * linearDensity / 840 / (shearCorrectionFactorVertical**2 + 2 * shearCorrectionFactorVertical + 1))
+SimplifyPrint(verticalBendingHeaveContributionMassMatrix, factor = l * linearDensity / 840 / (shearCorrectionFactorVertical**2 + 2 * shearCorrectionFactorVertical + 1))
 print('Pitch contribution:')
-PrettyPrint(verticalBendingPitchContributionMassMatrix, factor = (verticalMassInertiaMoment + linearDensity * (zCenterOfMass - zNeutralAxis)**2) / 30 / l / (shearCorrectionFactorVertical**2 + 2 * shearCorrectionFactorVertical + 1))
+SimplifyPrint(verticalBendingPitchContributionMassMatrix, factor = (verticalMassInertiaMoment + linearDensity * (zCenterOfMass - zNeutralAxis)**2) / 30 / l / (shearCorrectionFactorVertical**2 + 2 * shearCorrectionFactorVertical + 1))
 print()
 print('Axial-vertical bending coupling mass matrix:')
-PrettyPrint(axialVerticalBendingCouplingMassMatrix, factor = linearDensity * (zCenterOfMass - zNeutralAxis) / (12 * (1 + shearCorrectionFactorVertical)))
+SimplifyPrint(axialVerticalBendingCouplingMassMatrix, factor = linearDensity * (zCenterOfMass - zNeutralAxis) / (12 * (1 + shearCorrectionFactorVertical)))
 print()
 print('Horizontal bending mass matrix:')
-PrettyPrint(horizontalBendingMassMatrix, factor = l * linearDensity / 840 / (shearCorrectionFactorHorizontal**2 + 2 * shearCorrectionFactorHorizontal + 1))
+print('Sway contribution')
+SimplifyPrint(horizontalBendingSwayContributionMassMatrix, factor = l * linearDensity / 840 / (shearCorrectionFactorHorizontal**2 + 2 * shearCorrectionFactorHorizontal + 1))
+print('Yaw contribution')
+SimplifyPrint(horizontalBendingYawContributionMassMatrix, factor = horizontalMassInertiaMoment / 30 /l / (shearCorrectionFactorHorizontal**2 + 2 * shearCorrectionFactorHorizontal + 1))
 print()
 print('Torsion mass matrix:')
 print('Bounded basis:')
-PrettyPrint(massMatrixBoundedBasis)
+SimplifyPrint(massMatrixBoundedBasis, factor = rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2)
 print('Stable basis:')
 print('Free warping submatrix:')
-PrettyPrint(massMatrixStableBasis[0:2, 0:2])
+freeWarpingSubmatrix: sym.Matrix = sym.zeros(2, 2)
+freeWarpingSubmatrix[0, 0] = massMatrixStableBasis[0, 0]
+freeWarpingSubmatrix[1, 0] = massMatrixStableBasis[2, 0]
+freeWarpingSubmatrix[0, 1] = massMatrixStableBasis[0, 2]
+freeWarpingSubmatrix[1, 1] = massMatrixStableBasis[2, 2]
+SimplifyPrint(freeWarpingSubmatrix, factor = rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2)
 print('w0 w0:')
-PrettyPrint(massMatrixStableBasis[2, 2])
-PrettyPrint(sym.series(massMatrixStableBasis[2, 2], a, 0, 10))
+SimplifyPrint(massMatrixStableBasis[1, 1], factor = (rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2) * l, series = True, symbol = a)
 print('w0 w1:')
-PrettyPrint(massMatrixStableBasis[2, 3])
-PrettyPrint(sym.series(massMatrixStableBasis[2, 3], a, 0, 10))
+SimplifyPrint(massMatrixStableBasis[1, 3], factor = (rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2) * l, series = True, symbol = a)
 print('w1 w1:')
-PrettyPrint(massMatrixStableBasis[3, 3])
-PrettyPrint(sym.series(massMatrixStableBasis[3, 3], a, 0, 10))
+SimplifyPrint(massMatrixStableBasis[3, 3], factor = (rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2) * l, series = True, symbol = a)
 print('r0 w0:')
-PrettyPrint(massMatrixStableBasis[0, 2])
-PrettyPrint(sym.series(massMatrixStableBasis[0, 2], a, 0, 10))
+SimplifyPrint(massMatrixStableBasis[0, 1], factor = (rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2) * l, series = True, symbol = a)
 print('r0 w1:')
-PrettyPrint(massMatrixStableBasis[0, 3])
-PrettyPrint(sym.series(massMatrixStableBasis[0, 3], a, 0, 10))
-print('r1 w0:')
-PrettyPrint(massMatrixStableBasis[1, 2])
-PrettyPrint(sym.series(massMatrixStableBasis[1, 2], a, 0, 10))
+SimplifyPrint(massMatrixStableBasis[0, 3], factor = (rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2) * l, series = True, symbol = a)
+print('w0 r1:')
+SimplifyPrint(massMatrixStableBasis[1, 2], factor = (rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2) * l, series = True, symbol = a)
 print('r1 w1:')
-PrettyPrint(massMatrixStableBasis[1, 3])
-PrettyPrint(sym.series(massMatrixStableBasis[1, 3], a, 0, 10))
-
-# print()
-# print('Bending-torsion coupled mass matrix:')
-# PrettyPrint(bendingTorsionCoupledMassMatrixDisplacementBoundedBasis.subs(delta, 0))
+SimplifyPrint(massMatrixStableBasis[2, 3], factor = (rollMassInertiaMoment + linearDensity * (zCenterOfMass - zCenterOfTwist)**2) * l, series = True, symbol = a)
+print()
+print('Horizontal bending-torsion coupling mass matrix:')
+print('Bounded basis:')
+SimplifyPrint(horizontalBendingDispBasisTorsionBoundedBasisCouplingMassMatrix, factor = linearDensity * (zCenterOfMass - zCenterOfTwist))
+print('Stable basis:')
+freeWarpingCouplingSubmatrix = sym.zeros(4, 2)
+freeWarpingCouplingSubmatrix[:, 0] = horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[:, 0]
+freeWarpingCouplingSubmatrix[:, 1] = horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[:, 2]
+print('Free warping submatrix:')
+SimplifyPrint(freeWarpingCouplingSubmatrix, factor = linearDensity * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal) / 120)
+print('y0 w0')
+SimplifyPrint(horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[0, 1], factor = linearDensity * l * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal), series = True, symbol = a)
+print('psi0 w0')
+SimplifyPrint(horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[1, 1], factor = linearDensity * l**2 * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal), series = True, symbol = a)
+print('y1 w0')
+SimplifyPrint(horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[2, 1], factor = linearDensity * l * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal), series = True, symbol = a)
+print('psi1 w0')
+SimplifyPrint(horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[3, 1], factor = linearDensity * l**2 * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal), series = True, symbol = a)
+print('y0 w1')
+SimplifyPrint(horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[0, 3], factor = linearDensity * l * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal), series = True, symbol = a)
+print('psi0 w1')
+SimplifyPrint(horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[1, 3], factor = linearDensity * l**2 * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal), series = True, symbol = a)
+print('y1 w1')
+SimplifyPrint(horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[2, 3], factor = linearDensity * l * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal), series = True, symbol = a)
+print('psi1 w1')
+SimplifyPrint(horizontalBendingDispBasisTorsionStableBasisCouplingMassMatrix[3, 3], factor = linearDensity * l**2 * (zCenterOfMass - zCenterOfTwist) / (1 + shearCorrectionFactorHorizontal), series = True, symbol = a)
 
